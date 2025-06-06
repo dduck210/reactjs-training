8e61b47beeff22254e0f68aa80ac71d221f9033f
DUONG ANH DUC <ducda@OMINEXT.LOCAL>
2025-06-06 18:36:44 +0700

    Học thêm phần in ra toàn bộ phần id, name, age, description bằng map sử dụng template string

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const users = [
   },
   {
     id: 3,
-    name: "Dương Anh Đức",
+    name: "Dương Đức Anh",
     age: 21,
     description: "Lập trình viên frontend",
   },
@@ -27,8 +27,8 @@ const users = [
 ];
 
 // Lọc ra những người từ 18 tuổi trở lên
-const nguoiLonTuoi = users.filter((user) => user.age >= 18);
-console.log("Những người từ 18 tuổi trở lên là: ", nguoiLonTuoi);
+const nguoiLonTuoi = users.filter((user) => user.age >= 22);
+console.log("Mấy người đó là: ", nguoiLonTuoi);
 
 // Tìm ra những người có tên là Đức
 const nguoiTenDuc = users.filter((user) => user.name.includes("Đức")); // includes() dùng để kiểm tra xem một chuỗi (string) hoặc mảng (array) có chứa một phần tử nào đó hay k
@@ -36,6 +36,62 @@ console.log("Những người có tên là Đức là: ", nguoiTenDuc);
 
 // Tăng tuổi của tất cả thêm 2
 const tuoiTangLen2 = users.map((user) => {
-  return user.age + 2; // return trả về giá trị tuổi của mọi người bằng cách truy vấn vào user vào thuộc tính age xong +2 tuổi của mọi người
+  return {};
 });
-console.log("Tuổi của mọi người sau khi tăng thêm 2 là: ", tuoiTangLen2);
+
+// in ra thông tin tuổi và cả tên của mọi người sau khi tăng tuổi
+
+// const person = {
+//   name: "Nguyễn Văn A",
+//   age: 22,
+//   description: "Sinh viên ngành CNTT",
+// };
+// console.log(`${person.name} ${person.age + 2}`);
+
+users.map((getFull) => {
+  console.log(
+    `Tên: ${getFull.name}, Tuổi: ${getFull.age + 2}, Mô tả: ${
+      getFull.description
+    }`
+  );
+});
+// // sử dụng filter để in ra toàn bộ thông tin của người dùng sau khi tăng tuổi
+const allThongTinUsers = users.filter((user) => {
+  return {
+    id: user.id,
+    name: user.name,
+    age: user.age + 2,
+    description: user.description,
+  };
+});
+console.log("Thông tin all là: ", allThongTinUsers);
+
+const tenTuoiAllUsers = users.map((user) => {
+  return `${user.id} ${user.name} ${user.age + 2} ${user.description}`; // return trả về tên và tuổi của mọi người sau khi tăng tuổi
+});
+console.log("Tuổi là", tuoiTangLen2, tenTuoiAllUsers);
+
+const tuoiAllUsers1 = users.maps((user) => {
+  return {
+    id: user.id,
+    name: user.name,
+    age: user.age,
+    description: user.description,
+  };
+});
+console.log("Tuổi", tuoiAllUsers1);
+
+// ${user.name} - ${user.age
+
+const tuoiAllUser = users.maps((users) => {
+  return {
+    name: users.name,
+    age: users.age + 2,
+  };
+});
+console.log("Tuổi của all users sau khi tăng", tuoiAllUser);
+
+const tuoiAllUsers2 = users.maps((user) => {
+  return user.age + 2;
+});
+console.log("Tuổi của all users sau khi tăng", tuoiAllUsers2);
