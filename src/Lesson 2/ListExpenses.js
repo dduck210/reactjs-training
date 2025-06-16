37813f7fc6e2947118c77d0eaed3a6e9817ce438
DUONG ANH DUC <ducda@OMINEXT.LOCAL>
2025-06-16 14:19:59 +0700

    check git new part

---
+++ src/Lesson 2/ListExpenses.js
@@ -0,0 +1,17 @@
+// import React from "react";
+// import ExpenseItem from "./ExpenseItem";
+
+// const ListExpenses = (props) => {
+//   const data = props.data;
+
+//   return (
+//     <div>
+//       <ExpenseItem item={data[0]} />
+//       <ExpenseItem item={data[1]} />
+//       <ExpenseItem item={data[2]} />
+//       <ExpenseItem item={data[3]} />
+//     </div>
+//   );
+// };
+
+// export default ListExpenses;
--- src/Lesson2.js
+++
@@ -1,4 +0,0 @@
-function Lesson2() {
-  return <h2>Đây là website của tôi</h2>;
-}
-export default Lesson2;
